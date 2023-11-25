a9f22a6f24a11b5ddc3cea7740a094e458d46d7a
Martino <87116040+kruniak@users.noreply.github.com>
2023-11-25 20:04:04 +0100

    fixes

--- xp3tool.c
+++ xp3tool.c
@@ -1,8 +1,4 @@
-﻿//
-// kirikiri xp3 v1 unpacker/packer (currently only supporting morenatsu ver3.5 enc/dec, windows and linux)
-//
-// 2023 - kruniak
-//
+﻿// kirikiri xp3 v1 unpacker/packer (currently only supporting morenatsu ver3.5 enc/dec, windows and linux)
 
 #if defined(_MSC_VER)
 #define _CRT_SECURE_NO_WARNINGS
@@ -97,6 +93,7 @@
 #endif
 
 #define CHUNK_SIZE 4096
+#define XP3_ENCRYPTED_FLAG (1 << 31) // 0x80000000
 #define XP3_SEGMENT_SIZE 28
 
 const uint8_t XP3_MAGIC[] = { 'X', 'P', '3', '\r', '\n', 0x20, 0x0A, 0x1A, 0x8B, 0x67, 0x01 };
@@ -338,8 +335,67 @@ static void pack(const char* path)
     // TODO
 }
 
+// Decompresses zlib data from a buffer to out_file_path
+static void decompress_buffer_to_file(const unsigned char* buffer, size_t size, wchar_t* out_file_path)
+{
+    // Create an output file for writing decompressed data
+    FILE* output_file = WFOPEN(out_file_path, L"wb");
+    if (output_file == NULL)
+    {
+        ERR("Failed to open output file for writing.");
+        exit(1);
+    }
+
+    // Allocate buffer for decompressed data
+    unsigned char out_buf[CHUNK_SIZE];
+
+    // Init zlib stream
+    z_stream zstream;
+    zstream.zalloc = Z_NULL;
+    zstream.zfree = Z_NULL;
+    zstream.opaque = Z_NULL;
+
+    if (inflateInit(&zstream) != Z_OK)
+    {
+        ERR("Failed to initialize zlib.");
+        exit(1);
+    }
+
+    // Set input data
+    zstream.avail_in = (uInt)size;
+    zstream.next_in = (Bytef*)buffer;
+
+    int ret;
+
+    do
+    {
+        zstream.avail_out = CHUNK_SIZE;
+        zstream.next_out = (Bytef*)out_buf;
+
+        ret = inflate(&zstream, Z_NO_FLUSH);
+        if (ret == Z_NEED_DICT || ret == Z_DATA_ERROR || ret == Z_MEM_ERROR)
+        {
+            ERR("Failed to decompress data: %s", zstream.msg);
+            inflateEnd(&zstream);
+            fclose(output_file);
+
+            // XXX: Don't exit yet, some zlib compressed files have probably been tampered with
+            // to prevent extraction. (mostly .txt files)
+            // exit(1);
+        }
+
+        size_t have = CHUNK_SIZE - zstream.avail_out;
+
+        fwrite(out_buf, sizeof(char), have, output_file);
+
+    } while (zstream.avail_out == 0);
+
+    inflateEnd(&zstream);
+    fclose(output_file);
+}
+
 // Decompresses zlib data to out_file_path
-static void decompress_data(uint64_t data_offset, wchar_t* out_file_path)
+static void decompress_offset_to_path(uint64_t data_offset, wchar_t* out_file_path)
 {
     // Create an output file for writing decompressed data
     FILE* output_file = WFOPEN(out_file_path, L"wb");
@@ -452,7 +508,7 @@ static int unpack(const wchar_t* file_path, const wchar_t* output_dir)
     SWPRINTF(out_file_path, PATH_MAX, L"%ls.idx", file_path);
 
     // Decompress XP3 index to a file
-    decompress_data(hdr.index_offset + sizeof(xp3_index) + 1, out_file_path);
+    decompress_offset_to_path(hdr.index_offset + sizeof(xp3_index) + 1, out_file_path);
 
     // Read XP3 index file into a buffer
     FILE* idx_file = WFOPEN(out_file_path, L"rb");
@@ -479,6 +535,7 @@ static int unpack(const wchar_t* file_path, const wchar_t* output_dir)
     {
         xp3_entry entry = { 0 };
         parse_entry(&entry, idx_data, i);
+
         i += sizeof(xp3_entry);
 
         wchar_t file_path[2048] = { 0 };
@@ -498,6 +555,14 @@ static int unpack(const wchar_t* file_path, const wchar_t* output_dir)
 
         WLOG(L"Extracting %ls", file_path);
 
+        //if (!first && !(entry.encrypted & XP3_ENCRYPTED_FLAG))
+        //{
+        //    WERR(L"%ls is encrypted!", file_path);
+        //    return 1;
+        //}
+
+        int file_compressed_size = 0;
+        int segment_start_offset = i;
         xp3_segment segments[128] = { 0 };
         for (int j = 0; j < segments_num; j++)
         {
@@ -508,11 +573,11 @@ static int unpack(const wchar_t* file_path, const wchar_t* output_dir)
                 i += sizeof(xp3_segment);
 
                 first = false;
-                break;
+                goto skip_first;
             }
 
-            parse_segment(&segments[j], idx_data + i, j * sizeof(xp3_segment));
-            i += sizeof(xp3_segment);
+            parse_segment(&segments[j], idx_data + segment_start_offset, j * sizeof(xp3_segment));
+            // i += sizeof(xp3_segment);
 
             wchar_t file_dir_path[PATH_MAX] = { 0 };
             wchar_t out_file[PATH_MAX] = { 0 };
@@ -525,6 +590,10 @@ static int unpack(const wchar_t* file_path, const wchar_t* output_dir)
 
             wchar_t out_file_path[PATH_MAX] = { 0 };
             SWPRINTF(out_file_path, PATH_MAX, L"%ls" PATH_SEPARATOR L"%ls", out_dir, out_file);
+
+            fseek(g_file, (long)segments[j].offset, SEEK_SET);
+
+            fread(file_buf, sizeof(uint8_t), segments[j].decompressed_size, g_file);
 
             FILE* extracted_file = NULL;
             if (segments_num > 1)
@@ -542,15 +611,24 @@ static int unpack(const wchar_t* file_path, const wchar_t* output_dir)
                 return 1;
             }
 
-            fseek(g_file, (long)segments[j].offset, SEEK_SET);
-
-            fread(file_buf, sizeof(uint8_t), segments[j].decompressed_size, g_file);
             fwrite(file_buf, sizeof(uint8_t), segments[j].decompressed_size, extracted_file);
             fclose(extracted_file);
 
+            file_compressed_size += segments[j].compressed_size;
+
             fseek(g_file, 0, SEEK_SET);
+
+            // FIXME: this is a workaround. we should be able to determine
+            // if it's compressed or not beforehand
+            if (entry.compressed_size != entry.decompressed_size && j == (segments_num - 1))
+            {
+                decompress_buffer_to_file(file_buf, file_compressed_size, out_file_path);
+            }
         }
 
+        i += sizeof(xp3_segment) * segments_num;
+
+    skip_first:
         i += sizeof("adlr") - 1; // Skip 'adlr'
         i += sizeof(uint64_t);   // 04 00 00 00 00 00 00 00 00
 
